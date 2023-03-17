a74a28787b90cbb0b6f2340a5e6533183861bd8b
MemoBaca <memobaca97@gmail.com>
2023-03-17 18:07:09 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
+
+let cont = document.getElementById("container")
+
+let counter = 0
+let likes = []
+let hasLiked = []
+
+for (post of posts) {
+    cont.innerHTML += `
+        <section class="post">
+            <div class="subheader">
+                <img src="${post.avatar}" class="post-avatar">
+                <div class="post-text">
+                    <h1>${post.name}</h1>
+                    <h2>${post.location}</h2>
+                </div>
+            </div>
+            <img src="${post.post}" class="post-img">
+            <div class="icons">
+                <img src="images/icon-heart.png" class="post-icons">
+                <img src="images/icon-comment.png">
+                <img src="images/icon-dm.png">
+            </div>
+            <h3 class="post-likes">${post.likes} likes</h3>
+            <h4>${post.username} <span class="reg">${post.comment}</span></h4>
+        </section>`
+    likes.push(post.likes)
+    hasLiked.push(false)
+    counter++
+}
+
+const imgs = document.querySelectorAll('.post-img')
+const icons = document.querySelectorAll('.post-icons')
+const likesTexts = document.querySelectorAll('.post-likes')
+
+for (let i = 0; i < imgs.length; i++) {
+    imgs[i].addEventListener("dblclick", function () {  
+        updateLikes(i)
+    })
+    icons[i].addEventListener("dblclick", function () {
+        updateLikes(i)  
+    })
+  }
+
+  function updateLikes(postNum){
+    if (hasLiked[postNum]) {
+        likes[postNum]--
+        likesTexts[postNum].textContent = `${likes[postNum]} likes`
+        icons[postNum].src="images/icon-heart.png"
+        hasLiked[postNum] = !hasLiked[postNum]
+    }
+    else {
+        likes[postNum]++
+        likesTexts[postNum].textContent = `${likes[postNum]} likes`
+        icons[postNum].src="images/icon-heart2.png"
+        hasLiked[postNum] = !hasLiked[postNum]
+    }
+  }
+
+
