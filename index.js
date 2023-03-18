718c66b6c3dd0d736f791de9ee4c9dd661f983ca
MemoBaca <memobaca97@gmail.com>
2023-03-17 19:15:52 -0500

    Update links

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const posts = [
         name: "Vincent van Gogh",
         username: "vincey1853",
         location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
+        avatar: "https://raw.githubusercontent.com/MemoBaca/oldagram/main/images/avatar-vangogh.jpg",
+        post: "https://raw.githubusercontent.com/MemoBaca/oldagram/main/images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
         likes: 21
     },
@@ -12,8 +12,8 @@ const posts = [
         name: "Gustave Courbet",
         username: "gus1819",
         location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
+        avatar: "https://raw.githubusercontent.com/MemoBaca/oldagram/main/images/avatar-courbet.jpg",
+        post: "https://raw.githubusercontent.com/MemoBaca/oldagram/main/images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
         likes: 4
     },
@@ -21,8 +21,8 @@ const posts = [
         name: "Joseph Ducreux",
         username: "jd1735",
         location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
+        avatar: "https://raw.githubusercontent.com/MemoBaca/oldagram/main/images/avatar-ducreux.jpg",
+        post: "https://raw.githubusercontent.com/MemoBaca/oldagram/main/images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
         likes: 152
     }
@@ -46,9 +46,9 @@ for (let elements of posts) {
             </div>
             <img src="${elements.post}" class="post-img">
             <div class="icons">
-                <img src="images/icon-heart.png" class="post-icons">
-                <img src="images/icon-comment.png">
-                <img src="images/icon-dm.png">
+                <img src="https://raw.githubusercontent.com/MemoBaca/oldagram/main/images/icon-heart.png" class="post-icons">
+                <img src="https://raw.githubusercontent.com/MemoBaca/oldagram/main/images/icon-comment.png">
+                <img src="https://raw.githubusercontent.com/MemoBaca/oldagram/main/images/icon-dm.png">
             </div>
             <h3 class="post-likes">${elements.likes} likes</h3>
             <h4>${elements.username} <span class="reg">${elements.comment}</span></h4>
@@ -75,13 +75,13 @@ for (let i = 0; i < imgs.length; i++) {
     if (hasLiked[postNum]) {
         likes[postNum]--
         likesTexts[postNum].textContent = `${likes[postNum]} likes`
-        icons[postNum].src="images/icon-heart.png"
+        icons[postNum].src="https://raw.githubusercontent.com/MemoBaca/oldagram/main/images/icon-heart.png"
         hasLiked[postNum] = !hasLiked[postNum]
     }
     else {
         likes[postNum]++
         likesTexts[postNum].textContent = `${likes[postNum]} likes`
-        icons[postNum].src="images/icon-heart2.png"
+        icons[postNum].src="https://raw.githubusercontent.com/MemoBaca/oldagram/main/images/icon-heart2.png"
         hasLiked[postNum] = !hasLiked[postNum]
     }
   }
